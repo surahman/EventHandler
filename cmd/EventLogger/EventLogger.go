efb3ac102f1f2b59b6fa29ae162c1cdee37a9de3
Saad Ur Rahman <saadurrahman@apache.org>
2022-02-10 10:07:01 -0500

    [Structure] project structure fixed.

---
+++ cmd/EventLogger/EventLogger.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+    _ "REST_EventLogger_POC/pkg/EventLogger"
+)
+
+func main() {
+}
--- src/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-    _ "REST_EventLogger_POC/src/EventLogger"
-)
-
-func main() {
-}
